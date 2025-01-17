b03fb72b5d58e55ff5f2b94e08b88617cf83939c
Gautammeh <gautammehta092@gmail.com>
2025-01-17 17:01:26 +0530

    updated hamburger

--- script.js
+++ script.js
@@ -90,3 +90,32 @@ tl.from("#projects div",{
     }
 
 })
+
+
+const hamburger = document.getElementById("hamburger");
+const dropdown = document.getElementById("dropdown");
+
+
+
+let val = 0
+hamburger.addEventListener("click",()=>{
+
+    if(val== 0){
+
+        
+    
+    dropdown.style.display = "block";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-close-large-line");
+    val = 1;
+
+
+    }
+    else{
+    dropdown.style.display = "none";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-menu-line");
+    val = 0;
+
+    }  
+})
